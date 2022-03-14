501a46189307de1c2f0965276d45569c4a3572b8
nowgnas <marshmello.sw@gmail.com>
2022-03-15 01:14:30 +0900

    [amend] amend index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const { userRouter } = require("./server/routers/userRouter");
 
@@ -15,26 +14,15 @@ app.use(bodyParser.json());
 
 // .env 환경 변수 사용하기
 dotenv.config();
+// cors 사용: 자세히 알아보기
 app.use(cors());
 
 // > Routing
 app.get("/", (req, res) => {
-  res.send("안녕하세요, 레이서 프로젝트 API 입니다.");
+  res.send("backend root page");
 });
 
 app.use(userRouter);
-
-// ! db connect test
-// mongoose
-//   .connect(
-//     `mongodb+srv://admin:1234@simple-board-cluster.tsykx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-//   )
-//   .then(() => {
-//     console.log("db connect");
-//   })
-//   .catch((e) => {
-//     console.log(e.message);
-//   });
 
 // > open port
 const port = 5000;
